de676942636ee0d7ea0994cef91e3617ea09746b
Stephanie <stephaniiehallberg@gmail.com>
2022-07-15 10:59:00 +0200

    feat(gql-code)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
+
+import { upperDirectiveTransformer } from './common/directives/upper-case.directive';
 import { AppService } from './app.service';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      transformSchema: (schema) => upperDirectiveTransformer(schema, 'upper'),
+      installSubscriptionHandlers: true,
+      buildSchemaOptions: {
+        directives: [
+          new GraphQLDirective({
+            name: 'upper',
+            locations: [DirectiveLocation.FIELD_DEFINITION],
+          }),
+        ],
+      },
+    }),
+  ],
+  providers: [AppResolver, AppService],
 })
 export class AppModule {}
