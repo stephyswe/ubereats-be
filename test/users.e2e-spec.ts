31d893bf5c9aac13e15015fa511918c5156a50f9
Stephanie <stephaniiehallberg@gmail.com>
2022-07-23 19:36:31 +0200

    feat(tests): e2e - todo

---
+++ test/users.e2e-spec.ts
@@ -0,0 +1,69 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import * as request from 'supertest';
+import { PrismaService } from '../prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
+const testUser = {
+  email: 'nico@las.com',
+  password: '12345',
+};
+
+describe('UserModule (e2e)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwtToken: string;
+
+  const baseTest = () => request(app.getHttpServer()).post(GRAPHQL_ENDPOINT);
+  const publicTest = (query: string) => baseTest().send({ query });
+  const privateTest = (query: string) =>
+    baseTest().set('X-JWT', jwtToken).send({ query });
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+      providers: [PrismaService],
+    }).compile();
+
+    app = module.createNestApplication();
+
+    prisma = module.get<PrismaService>(PrismaService);
+
+    await app.init();
+  });
+
+  afterAll(async () => {
+    // getConnection().dropDatabase();
+    app.close();
+  });
+
+  describe('createAccount', () => {
+    it('should create account', () => {
+      return publicTest(`
+        mutation {
+          createAccount(input: {
+            email:"${testUser.email}",
+            password:"${testUser.password}",
+            role:Owner
+          }) {
+            ok
+            error
+          }
+        }
+        `)
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+  });
+});
