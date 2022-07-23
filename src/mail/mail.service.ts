032e53948080a741f64a85a7211c9d2e8687c59f
Stephanie <stephaniiehallberg@gmail.com>
2022-07-23 18:34:29 +0200

    feat(tests): mail & jwt

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -10,11 +10,11 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleOptions,
   ) {}
 
-  private async sendEmail(
+  async sendEmail(
     subject: string,
     template: string,
     emailVars: EmailVar[],
-  ) {
+  ): Promise<boolean> {
     const form = new FormData();
     form.append('from', `Uber Eats <mailgun@${this.options.domain}>`);
     form.append('to', `stephaniiehallberg@gmail.com`);
@@ -37,7 +37,7 @@ export class MailService {
         },
       );
     } catch (error) {
-      console.log(error);
+      return false;
     }
   }
 
