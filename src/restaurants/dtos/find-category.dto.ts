f3f4b8ccf4486581a188daaa717a76b0ae391df9
Stephanie <stephaniiehallberg@gmail.com>
2022-07-24 22:27:21 +0200

    feat(restaurants): all restaurants & pagination

--- src/restaurants/dtos/find-category.dto.ts
+++ src/restaurants/dtos/find-category.dto.ts
@@ -1,15 +1,21 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import {
+  PaginationInput,
+  PaginationOutput,
+} from '../../common/dtos/pagination.dto';
 import { Category } from '../models/category.model';
-import { CoreOutput } from './../../common/dtos/output.dto';
+import { Restaurant } from '../models/restaurant.model';
 
 @InputType()
-export class CategoryInput {
+export class CategoryInput extends PaginationInput {
   @Field(() => String)
   slug: string;
 }
 
 @ObjectType()
-export class CategoryOutput extends CoreOutput {
+export class CategoryOutput extends PaginationOutput {
+  @Field(() => [Restaurant], { nullable: true })
+  restaurants?: Restaurant[];
   @Field(() => Category, { nullable: true })
   category?: Category;
 }
