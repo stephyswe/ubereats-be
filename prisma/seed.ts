84b910aa5e1d03e8f089463a8306bed103f4e9f5
Stephanie <stephaniiehallberg@gmail.com>
2022-07-25 10:17:00 +0200

    feat(seed): seeding db

---
+++ prisma/seed.ts
@@ -0,0 +1,39 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+const userData: Prisma.UserCreateInput[] = [
+  {
+    email: 'user@email.com',
+    password: '$2a$10$TrsLWPbVb8QH5HZJ86ttyOHMXFpKA025T89ydK52U4mLNQm5oKd1a',
+    role: 'Owner',
+  },
+];
+
+async function main() {
+  console.log(`Start seeding ...`);
+  for (const u of userData) {
+    const user = await prisma.user.create({
+      data: u,
+    });
+    console.log(`Created user with id: ${user.id}`);
+
+    await prisma.verification.create({
+      data: {
+        code: 'b22ed89a-6ba8-4e5d-8a57-2a2d19dd2ae4',
+        userId: user.id,
+      },
+    });
+  }
+  console.log(`Seeding finished.`);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
