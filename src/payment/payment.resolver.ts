78200ff73851bc6e641d29e80136383b49834399
Stephanie <stephaniiehallberg@gmail.com>
2022-07-29 20:15:39 +0200

    feat(payment): find payments

--- src/payment/payment.resolver.ts
+++ src/payment/payment.resolver.ts
@@ -1,10 +1,11 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { User } from '@prisma/client';
 
 import { Role } from 'src/auth/role.decorator';
 import { CurrentUser } from '../auth/auth-user.decorator';
 
 import { CreatePaymentInput, CreatePaymentOutput } from './dtos/create-payment.dto';
+import { FindManyPaymentOutput } from './dtos/find-payments.dto';
 import { Payment } from './models/payment.model';
 import { PaymentService } from './payment.service';
 
@@ -12,6 +13,12 @@ import { PaymentService } from './payment.service';
 @Resolver(() => Payment)
 export class PaymentResolver {
   constructor(private readonly paymentService: PaymentService) {}
+
+  @Query(() => FindManyPaymentOutput)
+  @Role(['Owner'])
+  findManyPayments(@CurrentUser() user: User) {
+    return this.paymentService.findManyPayments(user);
+  }
 
   @Mutation(() => CreatePaymentOutput)
   @Role(['Owner'])
