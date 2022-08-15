a7cdb930b5abf325db492ff494b33717d4573ff8
Stephanie <stephaniiehallberg@gmail.com>
2022-08-15 12:43:14 +0200

    feat(deploy): profile & validate envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,12 @@ import { UsersModule } from './users/users.module';
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         PRIVATE_KEY: Joi.string().required(),
+        POSTGRES_URL: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN: Joi.string().required(),
+        MAILGUN_FROM_EMAIL: Joi.string().required(),
+        AWS_KEY: Joi.string().required(),
+        AWS_SECRET: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
